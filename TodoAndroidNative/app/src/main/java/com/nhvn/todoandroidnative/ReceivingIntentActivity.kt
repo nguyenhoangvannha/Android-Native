4fccecf9e5b2cc743b6cf84c19351b4a395ebddf
nguyenhoangvannha <nguyenhoangvannha@gmail.com>
2021-12-21 16:46:09 +0700

    Receive dial from intent filter

--- TodoAndroidNative/app/src/main/java/com/nhvn/todoandroidnative/ReceivingIntentActivity.kt
+++ TodoAndroidNative/app/src/main/java/com/nhvn/todoandroidnative/ReceivingIntentActivity.kt
@@ -1,11 +1,20 @@
 package com.nhvn.todoandroidnative
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class ReceivingIntentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_receiving_intent)
+
+        if (intent?.action == Intent.ACTION_DIAL) {
+            val extras = intent.extras;
+            findViewById<TextView>(R.id.txtText).text = extras.toString()
+            Log.i("ReceivingIntentActivity", extras.toString());
+        }
     }
 }
