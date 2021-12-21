e44d278e3b8214974f5c743c0aab56a4d217db5e
nguyenhoangvannha <nguyenhoangvannha@gmail.com>
2021-12-21 16:32:19 +0700

    Receive contact info when get contact intent result

---
+++ TodoAndroidNative/app/src/main/java/com/nhvn/todoandroidnative/ReceivingIntentActivity.kt
@@ -0,0 +1,11 @@
+package com.nhvn.todoandroidnative
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReceivingIntentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_receiving_intent)
+    }
+}
