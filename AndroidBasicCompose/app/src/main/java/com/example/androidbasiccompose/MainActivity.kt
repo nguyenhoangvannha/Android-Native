d1e7114fe378e7e14911c036e9904bb1db0b63d8
NHV Nha <nguyenhoangvannha@gmail.com>
2024-01-28 18:33:51 +0700

    Dice Roller IBET APP

--- AndroidBasicCompose/app/src/main/java/com/example/androidbasiccompose/MainActivity.kt
+++ AndroidBasicCompose/app/src/main/java/com/example/androidbasiccompose/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.androidbasiccompose.birthday.BirthDayCard
+import com.example.androidbasiccompose.compose.diceroller.DiceRollerApp
 import com.example.androidbasiccompose.compose.practice.ComposeQuadrant
 import com.example.androidbasiccompose.ui.theme.AndroidBasicComposeTheme
 
@@ -16,26 +17,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AndroidBasicComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    ComposeQuadrant(
-                        titleList = listOf(
-                            "Text composable",
-                            "Image composable",
-                            "Row composable",
-                            "Column composable"
-                        ),
-                        desList = listOf(
-                            "Displays text and follows the recommended Material Design guidelines.",
-                            "Creates a composable that lays out and draws a given Painter class object.",
-                            "A layout composable that places its children in a horizontal sequence.",
-                            "A layout composable that places its children in a vertical sequence.",
-                        )
-                    )
-                }
+                DiceRollerApp()
             }
         }
     }
--- AndroidBasicCompose/app/src/main/java/com/example/androidbasiccompose/compose/practice/ComposeQuadrant.kt
+++ AndroidBasicCompose/app/src/main/java/com/example/androidbasiccompose/compose/practice/ComposeQuadrant.kt
@@ -11,11 +11,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.androidbasiccompose.R
 import com.example.androidbasiccompose.ui.theme.AndroidBasicComposeTheme
 
 @Composable
@@ -83,19 +85,20 @@ fun Quadrant(first: String, first1: String, color: Color, modifier: Modifier = M
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun UI2Preview() {
+
     AndroidBasicComposeTheme {
         ComposeQuadrant(
             titleList = listOf(
-                "Text composable",
-                "Image composable",
-                "Row composable",
-                "Column composable"
+                stringResource(R.string.text_composable),
+                stringResource(R.string.image_composable),
+                stringResource(R.string.row_composable),
+                stringResource(R.string.column_composable)
             ),
             desList = listOf(
-                "Displays text and follows the recommended Material Design guidelines.",
-                "Creates a composable that lays out and draws a given Painter class object.",
-                "A layout composable that places its children in a horizontal sequence.",
-                "A layout composable that places its children in a vertical sequence.",
+                stringResource(R.string.displays_text_and_follows_the_recommended_material_design_guidelines),
+                stringResource(R.string.creates_a_composable_that_lays_out_and_draws_a_given_painter_class_object),
+                stringResource(R.string.a_layout_composable_that_places_its_children_in_a_horizontal_sequence),
+                stringResource(R.string.a_layout_composable_that_places_its_children_in_a_vertical_sequence),
             )
         )
     }
